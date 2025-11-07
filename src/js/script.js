8a0371c0e1217208bcc6cb80a08aed58c3f4603d
Valder004 <valentin.deroo@edu.univ-fcomte.fr>
2025-11-07 14:41:03 +0100

    TP1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 // TODO
+const texte = document.querySelector("#a-mettre-en-rouge");
+texte.style.color="red";
+
+const button = document.querySelector("#en-rouge-suite-a-click");
+button.addEventListener("click" ,(evt)=>{button.style.color="red";})
+
+const H2 = document.querySelectorAll("h2");
+H2.forEach((elm)=>{elm.addEventListener("click", (evt)=>{evt.target.style.color = "red"; 
+});
+});
